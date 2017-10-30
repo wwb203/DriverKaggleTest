29087fc8931c41aced157411e09a31c76bb50fae
Hua Wei <hua.wei@email.ucr.edu>
2017-10-30 16:59:47 -0700

    add

--- Kernel.py
+++ Kernel.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import json
 
 from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import StandardScaler
 # This class is for the preprocessing, take csv path as input, and aim to return a pandas data frame for trainning
 class PreProcessing:
 
@@ -11,13 +13,38 @@ class PreProcessing:
 
   # This method have deal with missing data before merge or drop
   def MissingData(self):
-    #self.df = self.df.replace(-1, np.NaN)
-    #df[""].fillna(df[""].mean(), inplace=True)
-    #df[""].fillna(df[""].median(), inplace=True)
-    #df[""].fillna(df[""].value_counts().idxmax(), inplace=True)
-    #mean_imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-    #mdan_imp = Imputer(missing_values='NaN', strategy='median', axis=0)
-    #mfrq_imp = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+    self.df = self.df.replace(-1, np.NaN)
+    #print (self.df.columns[self.df.isnull().any()])
+    '''
+    'ps_ind_02_cat', 'ps_ind_04_cat', 'ps_ind_05_cat', 
+    'ps_reg_03',
+    'ps_car_01_cat', 'ps_car_02_cat', 'ps_car_03_cat', 'ps_car_05_cat', 'ps_car_07_cat', 'ps_car_09_cat', 
+    'ps_car_11', 'ps_car_12', 'ps_car_14'
+    '''
+    mean_imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    mdan_imp = Imputer(missing_values='NaN', strategy='median', axis=0)
+    mfrq_imp = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+
+    self.df["ps_ind_02_cat"].fillna(-1, inplace=True)
+    self.df["ps_ind_04_cat"].fillna(-1, inplace=True)
+    self.df["ps_ind_05_cat"].fillna(-1, inplace=True)
+    #self.df["ps_reg_03"].fillna(self.df["ps_reg_03"].median(), inplace=True)
+    self.df['ps_reg_03'] = mean_imp.fit_transform(self.df[['ps_reg_03']]).ravel()
+    self.df["ps_car_01_cat"].fillna(-1, inplace=True)
+    self.df["ps_car_02_cat"].fillna(-1, inplace=True)
+    self.df["ps_car_03_cat"].fillna(self.df["ps_car_03_cat"].value_counts().idxmax(), inplace=True) # top 1 missing variable, drop
+    self.df["ps_car_05_cat"].fillna(self.df["ps_car_05_cat"].value_counts().idxmax(), inplace=True) # top 2 missing variable, drop
+    self.df["ps_car_07_cat"].fillna(-1, inplace=True)
+    self.df["ps_car_09_cat"].fillna(-1, inplace=True)
+    #self.df["ps_car_11"].fillna(self.df["ps_car_11"].value_counts().idxmax(), inplace=True)
+    self.df['ps_car_11'] = mfrq_imp.fit_transform(self.df[['ps_car_11']]).ravel()
+    #self.df["ps_car_12"].fillna(self.df["ps_car_12"].median(), inplace=True)
+    self.df['ps_car_12'] = mean_imp.fit_transform(self.df[['ps_car_12']]).ravel()
+    #self.df["ps_car_14"].fillna(self.df["ps_car_14"].median(), inplace=True)
+    self.df['ps_car_14'] = mean_imp.fit_transform(self.df[['ps_car_14']]).ravel()
+    #self.df[""].fillna(self.df[""].mean(), inplace=True)
+    #self.df[""].fillna(self.df[""].median(), inplace=True)
+    #self.df[""].fillna(self.df[""].value_counts().idxmax(), inplace=True)
     return
 
   # This method drop or merge variables in dataframe accroding to corr map
@@ -39,16 +66,22 @@ class PreProcessing:
     self.df.drop(['ps_ind_16_bin', 'ps_ind_17_bin', 'ps_ind_18_bin'], axis = 1, inplace = True)
  
     # drop this variable from preprocessing study, top 3 missing data, and not important at all
-    #self.df.drop(['ps_car_05_cat'], axis = 1, inplace = True)
+    self.df.drop(['ps_car_03_cat'], axis = 1, inplace = True)
+    self.df.drop(['ps_car_05_cat'], axis = 1, inplace = True)
    
     #self.df['ps_car_13'] = (self.df['ps_car_13']*self.df['ps_car_13']*48400).round(0)
     #self.df['ps_car_12'] = (self.df['ps_car_12']*self.df['ps_car_12']).round(4) * 10000
     return
 
+  def ScaleFeatures(self):
+    scaler = StandardScaler(copy=False)
+    scaler.fit_transform(self.df.drop(['id','target'], axis=1))
+
   # this method pack all previous preprocessing all together and return the data frame
   def FinalFrameforTrainning(self):
     self.MissingData()
-    #self.CorrMergeDrop()
+    self.CorrMergeDrop()
+    self.ScaleFeatures()
     return self.df
 
 from sklearn.metrics import roc_auc_score
@@ -60,6 +93,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
+from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
 # this class define various trainning methods, also cross validation to tune the hyper parameters
 class Train:
@@ -78,13 +112,29 @@ class Train:
                                                         random_state=0
                                                        )
 
-    sc_mod = RandomForestClassifier(n_estimators=500, max_features='auto', criterion='gini', max_depth=10, min_samples_leaf=10, min_samples_split=15, n_jobs=-1, random_state=0)
+    sc_mod = RandomForestClassifier(n_estimators=500, criterion='gini', max_features='auto', max_depth=10, min_samples_split=15, min_samples_leaf=10, n_jobs=-1, random_state=0)
     sc_mod.fit(X_train, y_train)
     ysc_pred = sc_mod.predict_proba(X_test)[:,1]
     #print (ysc_pred)
     #print (y_test)
     gini = 2*roc_auc_score(y_test, ysc_pred)-1
     return gini
+
+  def rf_param_selection(self, nfolds):
+    sc_mod = RandomForestClassifier(criterion='gini', max_depth=None, min_samples_split=2, min_samples_leaf=1, n_jobs=-1, random_state=0)
+    nests = [400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000]
+    mfeat = [6, 7, 8, 9, 10, 11]
+    #nests = [300]
+    #mfeat = [6]
+    #mdeps = [8, 9, 10, 11, 12, 13, 14]
+    param_grid = {'n_estimators': nests, 'max_features' : mfeat}
+    grid_search = GridSearchCV(sc_mod, param_grid, cv=nfolds)
+    grid_search.fit(self.df.drop(['id', 'target'],axis=1), self.df.target)
+    #print (grid_search.cv_results_)
+    print (grid_search.grid_scores_)
+    #print (grid_search.best_params_)
+    return grid_search.cv_results_
+    #return grid_search.best_params_
 
   # A simple train of random forest model with scikit learn
   def TrainSKLearnRandomForest(self):
@@ -158,7 +208,13 @@ if __name__ == '__main__':
   #test_p = pd.read_csv('test_p.csv')
   
   train = Train(train_p)
-  print (train.PreprocessingScore())
+  #print (train.PreprocessingScore())
+  scan_res = train.rf_param_selection(2)
+  #j = json.dumps(scan_res, indent=2)
+  #f = open('sample.json', 'w')
+  #print >> f, j
+  #f.close()
+  #np.savetxt('PgridScan.txt', train.rf_param_selection(2))
   '''
   thisrf = train.TrainSKLearnRandomForest()
   #thisxgb = train.TrainXGBoost()
